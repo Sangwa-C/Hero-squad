3aa5b4f9605c10d26f2797db6911d762032c23c2
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-09-14 04:50:11 +0200

    changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "home.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
+        //post: process new post form
+//        post("/adding", (request, response) -> { //URL to make new post on POST route
+//            Map<String, Object> model = new HashMap<>();
+//            String content = request.queryParams("content");
+//            Hero newHero = new Hero(content);
+//            model.put("name", newHero);
+//            return new ModelAndView(model, "addhero.hbs");
+//        }, new HandlebarsTemplateEngine());
+
+        //get: show all posts
+//        get("/", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            ArrayList<Hero> posts = Hero.getAll();
+//            model.put("posts", posts);
+
+//        get("/greeting_card", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            String recipient = request.queryParams("recipient");
+//            String sender = request.queryParams("sender");
+//            model.put("recipient", recipient);
+//            model.put("sender", sender);
+//            return new ModelAndView(model, "greeting_card.hbs");
+//        }, new HandlebarsTemplateEngine());
+    }
 }
--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,24 @@
 package models;
+import java.util.ArrayList;
 
 public class Hero {
+    private String name;
+    private String age;
+    private String power;
+    private String weakness;
+
+    public Hero (String content) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+
+//: At the very least, they should have a name, age, special power and weakness
+    }
+
+    public String getname(){
+        return name;
+    }
+
 }
+
